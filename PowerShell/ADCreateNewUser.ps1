6d78df06d95391a413990ba84930499be23806f8
ActionJackson <marius.moosbauer@bbqberlin.onmicrosoft.com>
2021-06-08 21:33:57 +0200

    dev

---
+++ PowerShell/ADCreateNewUser.ps1
@@ -0,0 +1,91 @@
+﻿Clear-Host
+<#--------------AD Create new users from .csv--------------
+Hot to Use:
+Edit the filename and OUs for groups and users to your corresponding ldap path. Change the 
+$domainName and the variables corresponding to your .csv file. 
+
+Make sure .csv is UTF-8 and not ANSI.
+#>
+
+# Filename
+[string] $filename = "import.csv"
+
+# User and Groups Organizazional Units | LDAP path
+[string] $userOU = "OU=OU_Users,DC=moo,DC=zz"
+[string] $groupOU = "OU=OU_Groups,DC=moo,DC=zz"
+
+# Objects to select from .csv file | DO NOT USE "E-Mail" as variable for an Object it is an internal PowerShell function.
+[string] $domainName = "@moo.zz"
+[string] $delimiter = ";"
+[string] $firstname = "vorname"
+[string] $lastname = "nachname"
+[string] $login = "login"
+[string] $name = "name"
+[string] $course = "kurs"
+[string] $password = "passwort"
+
+# Counter for metrics and debugging
+[int32] $totalcount = 0
+[int32] $groupCount = 0
+[int32] $userCount = 0
+# [int32] $userFailCount = 0
+# [int32] $groupFailCount = 0
+
+Write-Host "`n----------------------------WORKING----------------------------" -ForegroundColor Yellow
+# Function for adding the users
+function AddUserToAD {
+    Import-Csv ".\$filename" -Delimiter $delimiter | Select-Object $firstname, $lastname, $login, $name, $course, $password | ForEach-Object {
+        $totalcount++
+
+        # Define Loop Variables
+        $fname = $_.$firstname
+        $lname = $_.$lastname
+        $SAM = $_.$login
+        $fullname = $_.$name
+        $courseID = $_.$course
+        $pass = $_.$password
+        
+        # Create UPN
+        $UPN = $SAM + $domainName
+       
+        # Securing the Passwd 
+        $securePass = $pass | ConvertTo-SecureString -AsPlainText -Force
+    
+        # Creating a new Group and ADUser | Catching User exists and Groups exists exceptions
+        try {   
+            New-ADGroup -Name "$courseID" -GroupCategory Security -GroupScope Global -SamAccountName "$courseID" -path "$groupOU" -DisplayName "$courseID"           
+            $groupCount++
+            Write-Host "Group: $courseID created."
+        }
+        catch {
+            Write-Host "New-ADGroup : Gruppe: $courseID ist bereits vorhanden" -ForegroundColor Yellow
+            # $groupFailCount++           
+        }
+        try {
+            New-ADUser -Name "$fullname" -GivenName "$fname" -Surname "$lname" -SamAccountName "$SAM" -UserPrincipalName "$UPN" -Path "$userOU" -Description "$courseID" -AccountPassword($securePass) -Enabled $true -CannotChangePassword $true -PasswordNeverExpires $true <#-Server $serverName -ChangePasswordAtLogon $true#> 
+            Add-ADPrincipalGroupMembership -Identity $SAM -MemberOf $courseID
+            $userCount++
+            Write-Host "Count[$totalcount] User: $fullname created."         
+        }
+        catch {
+            Write-Host "New-ADUser : Konto: $fullname ist bereits vorhanden" -ForegroundColor Yellow        
+            # $userFailCount++
+        }      
+        
+        <# Debug output      
+        Write-Host "Count[$totalcount]>--------------------------------------------------------->$UPN " -ForegroundColor Green
+        Get-ADUser $SAM    
+        Get-ADGroup $courseID
+        #>     
+    }
+    # Summary 
+    Write-Host "`n----------------------------SUMMARY----------------------------" -ForegroundColor Yellow
+    Write-Host "`nUsers added: $userCount / $totalcount" -ForegroundColor Green   
+    # Write-Host "Failed: $userFailCount" -ForegroundColor Red
+    Write-Host "`nGroups added: $groupCount`n" -ForegroundColor Green 
+    # Write-Host "Failed: $groupFailCount" -ForegroundColor Red
+    Write-Host "---------------------------------------------------------------" -ForegroundColor Yellow
+    Write-Host "REMOVE THE import.csv after use to avoid confusion." -ForegroundColor Red
+    Write-Host "---------------------------------------------------------------" -ForegroundColor Yellow
+}
+AddUserToAD;
---
+++ PowerShell/CreateNewDataFromCSVRandomPW.ps1
@@ -0,0 +1,131 @@
+﻿<# ------Create Student Data from CSV file ------
+How to use:
+Rename the $filename to your needs. If necessary replace the Objects within the functions 
+to firstname and lastname corresponding to cour .csv file. Change the @domain.de to your
+corresponding domain.
+
+The script will generate a .csv file when finished in the same folder. 
+The name of the file is the current date.
+
+Make sure your .csv file is in the same folder. As well as the shell. Prepare your.csv file.
+
+!IMPORTANT!
+If in your .csv file there is a row called "E-Mail", change it to Mail. PowerShell has does not recognize E-Mail
+since its an built-in function. Editing .csv with EXCEL can lead to formating issues with vowls. Also, excel uses
+a different limiter like ";", this will also lead to importing issues. The standard delmimiter should be an ",".
+
+Modify the Objects to select from .csv file starting line 30.
+
+Moezy
+#>
+Clear-Host
+# Filename
+[string] $filename = "testdata.csv"
+
+$date = Get-Date -Format yyyy-MM-dd-HH-mm-ss
+# Set Date and Filename
+$csvFileDB = $date
+$csvFileAD = "import.csv"
+
+#Initial CSV format
+[string] $csvFormatDB = "fullname;username;firstname;lastname;ad;email;password"
+[string] $csvFormatAD = "name;login;vorname;nachname;passwort;kurs"
+
+#Append Format to file
+$csvFormatDB >> .\"$csvFileDB.csv"
+$csvFormatAD >> .\"$csvFileAD"
+
+# Objects to select from .csv file | DO NOT USE "E-Mail" as Object it is an internal PowerShell function.
+$delimiter = ";"
+$firstname = "Vorname"
+$lastname = "Nachname"
+$email = "Mail"
+$course = "Kurs"
+
+# Counter
+$count = 0
+$countCrt = 0
+
+# function to create student data 
+function CreateStudentData {
+    Import-Csv ".\$filename" -Delimiter $delimiter | Select-Object $firstname, $lastname, $email, $course | ForEach-Object {
+        $count = $count + 1
+        # Vowl conversion
+        $_.$firstname = $_.$firstname.Replace("Ö", "Oe")
+        $_.$firstname = $_.$firstname.Replace("Ä", "Ae")
+        $_.$firstname = $_.$firstname.Replace("Ü", "Ue")
+        $_.$firstname = $_.$firstname.Replace("ö", "oe")
+        $_.$firstname = $_.$firstname.Replace("ä", "ae")
+        $_.$firstname = $_.$firstname.Replace("ü", "ue")
+        $_.$firstname = $_.$firstname.Replace("ß", "ss")
+        $_.$firstname = $_.$firstname.Replace(" ", "-")
+
+        $_.$lastname = $_.$lastname.Replace("Ö", "Oe")
+        $_.$lastname = $_.$lastname.Replace("Ä", "Ae")
+        $_.$lastname = $_.$lastname.Replace("Ü", "Ue")
+        $_.$lastname = $_.$lastname.Replace("ö", "oe")
+        $_.$lastname = $_.$lastname.Replace("ä", "ae")
+        $_.$lastname = $_.$lastname.Replace("ü", "ue")
+        $_.$lastname = $_.$lastname.Replace("ß", "ss")
+        $_.$lastname = $_.$lastname.Replace(" ", "-")
+        
+        # Create Login and Mail from Substring
+        $surname = $_.$lastname
+        $name = $_.$firstname
+        $privMail = $_.$email
+        $courseID = $_.$course
+
+        # Create User Data
+        $fullName = $_.$firstname + " " + $_.$lastname 
+        $login = $_.$firstname.Substring(0, 1) + "." + $_.$lastname
+        $activDir = $_.$firstname.Substring(0, 1) + "." + $_.$lastname + "@domain.local"
+        
+        # Random Numbers for Passwd
+        $num1 = Get-Random -Minimum 10 -Maximum 100
+        $num2 = Get-Random -Minimum 10 -Maximum 100
+
+        # Roll indexes for character (re)placement
+        $randIndexLower = Get-Random -Minimum 0 -Maximum 24
+        $randIndexUpper = Get-Random -Minimum 0 -Maximum 24
+        $randIndexLower2 = Get-Random -Minimum 0 -Maximum 24
+        $randIndexUpper2 = Get-Random -Minimum 0 -Maximum 24
+
+        # Roll characters exclude I and l
+        $rplUpper = [char[]]([char]'A'..[char]'H' + [char]'J'..[char]'Z')
+        $rplLower = [char[]]([char]'a'..[char]'k' + [char]'m'..[char]'z')
+
+        # Piece Passwd together -> can be better with replace with random character
+        $pass = $rplUpper[$randIndexUpper] + $rplLower[$randIndexLower] + $num1 + "#" + $rplUpper[$randIndexUpper2] + $rplLower[$randIndexLower2] + $num2 + "*"
+
+        # Append to File as CSV     
+        $fullName + ";" + $login.ToLower() + ";" + $name + ";" + $surname + ";" + $activDir + ";" + $privMail + ";" + $pass >> .\"$csvFileDB.csv"
+        $fullName + ";" + $login.ToLower() + ";" + $name + ";" + $surname + ";" + $pass + ";" + $courseID >> .\"$csvFileAD"   
+
+    }
+
+    # Count entires in created .csv file
+    $countCreated = Import-Csv ".\$csvFileDB.csv" | Select-Object $firstname | ForEach-Object {
+        $countCrt = $countCrt + 1 }
+    
+    # Status Message
+    if ($countCrt -eq $count) {
+        $outInfo = "Okay"
+    }
+    elseif ($countCrt -lt $count) {
+        $outInfo = "Not all files were transfered!`nCheck the syntax of the file or script."
+    }
+
+    # Summary
+    Clear-Host
+    Write-Host "Summary"
+    Write-Host "----------------------------------------------------------------" -ForegroundColor Yellow
+    Write-Host "Status:" $outInfo
+    Write-Host "Entries created:" $countCrt"/"$count -ForegroundColor Green
+    Write-Host "Source filename:"$filename
+    Write-Host "DB Output filename:"$csvFileDB".csv"
+    Write-Host "AD Output filename:"$csvFileAD
+    Write-Host "---------------------------------------------------------------`n" -ForegroundColor Yellow
+    
+}
+# Run function
+CreateStudentData;
